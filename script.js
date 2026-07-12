1c47ea7939bab485d2f7a7301667fa5b1122e16f
apenas1jao <joaomarcosandrade74@gmail.com>
2026-07-12 03:03:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const params = new URLSearchParams(window.location.search);
 
-// Se não passar ?start=..., usa este horário padrão.
-const startParam = params.get("start") || "2026-07-12T15:00:00-03:00";
+// Data e hora em que a live termina
+const endParam = params.get("end") || "2026-07-19T15:00:00";
 
-const inicio = new Date(startParam);
+const fim = new Date(endParam);
 
 function atualizar(){
 
     const agora = new Date();
 
-    let segundos = Math.floor((agora - inicio)/1000);
+    let segundos = Math.floor((fim - agora)/1000);
 
     if(segundos < 0){
         segundos = 0;
